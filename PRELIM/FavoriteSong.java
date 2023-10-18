77fa6b37e3174767b8b5a9e5be4a0bd16e7c3de6
jumelabe <148032922+jumelabe@users.noreply.github.com>
2023-10-18 06:26:50 +0100

    Add files via upload

---
+++ PRELIM/FavoriteSong.java
@@ -0,0 +1,24 @@
+/*Name: Jumel Anthony Labe
+  Yr. & Sec: BSIT - 2B
+  Activity 1 */
+ 
+public class FavoriteSong {
+   public static void main(String[] args) {
+   
+ 
+   
+      
+  System.out.println("Nang ikaw ibigin ko");
+  System.out.println("Mundo koy biglang nagbago");
+  System.out.println("di mo alam dahil");
+  System.out.println("sayo ");
+
+   
+   }
+   }
+  
+    
+
+
+
+
---
+++ PRELIM/PEMDASCalculator.java
@@ -0,0 +1,21 @@
+/*Name: Jumel Anthony Labe
+  Yr. & Sec: BSIT - 2B
+  Activity 1 */
+import java.util.Scanner;
+public class PEMDA {
+   public static void main(String[] args) {
+   Scanner scanner = new Scanner(System.in);
+   
+  System.out.println("Welcom to the PEMDAS Calculator");
+  System.out.println("Enter a mathematical expression");
+  
+  String expression = Scanner
+   
+   }
+   }
+  
+    
+
+
+
+
